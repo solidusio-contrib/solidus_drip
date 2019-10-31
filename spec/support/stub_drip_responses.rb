404136a0fede498abe19548dda55a1faa8e2d919
Eric Saupe <ericsaupe@gmail.com>
2019-10-31 15:31:34 -0700

    Stubs Drip API requests

---
+++ spec/support/stub_drip_responses.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Stub requests to Drip API
+RSpec.configure do |config|
+  config.before(:each) do
+    response = Drip::Response.new(200, {
+                                    "request_id": "990c99a7-5cba-42e8-8f36-aec3419186ef"
+                                  })
+    allow_any_instance_of(Drip::Client).to receive(:create_cart_activity_event).and_return(response)
+    allow_any_instance_of(Drip::Client).to receive(:create_order_activity_event).and_return(response)
+  end
+end
